9e9a2e6189dff86acfa9af30df8504bd3e8805c4
Johny C <chemicalsequence@yahoo.com>
2016-02-27 22:25:11 +0530

    Main php and readme files added according to nomenclature. Readme.txt yet to be filled properly.

---
+++ juggle-aon.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Juggler by AON
+Plugin URI: http://github.com/johnycage/juggle-aon/
+Description: Juggler is WordPress plugin to simplify the management of multiple sites handled by Agencies or webmasters.
+Author: Johny Cage	
+Version: 0.1
+*/
+?>
+
