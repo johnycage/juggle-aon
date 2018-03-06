a3ba40bf5e3326281cc9f30e7b04c0d3fc5f8ea7
Johny C <chemicalsequence@yahoo.com>
2018-03-06 22:29:57 +0530

    added url
    added url

--- juggle-aon.php
+++ juggle-aon.php
@@ -4,7 +4,8 @@ Plugin Name: Juggle by AON
 Plugin URI: http://github.com/johnycage/juggle-aon/
 Description: "Juggle by AON" is WordPress plugin to simplify the management of multiple sites handled by Agencies or webmasters.
 Author: Johny Cage	
-Version: 0.1
+Author URI: http://pranav.amrute.me/
+Version: 0.2
 */
 
 ?>
